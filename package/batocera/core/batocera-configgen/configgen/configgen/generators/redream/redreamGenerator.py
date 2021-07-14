f6ae5fced272567fef81b29662ebac5ac72d8d44
liberodark <liberodark@gmail.com>
2021-07-14 22:16:34 +0200

    Bump Redream

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/redream/redreamGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/redream/redreamGenerator.py
@@ -5,19 +5,25 @@ from generators.Generator import Generator
 import controllersConfig
 from shutil import copyfile
 import os
+import filecmp
+
+redream_file = "/usr/bin/redream"
+redreamConfig = batoceraFiles.CONF + "/redream"
 
 class RedreamGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
+        redream_exec = redreamConfig + "/redream"
 
         configdir = "/userdata/system/configs/redream"
         if not os.path.exists(configdir):
             os.makedirs(configdir)
 
-        copyfile("/usr/bin/redream", configdir + "/redream")
-        os.chmod(configdir + "/redream", 0o0775)
+        copyfile(redream_file, redream_exec)
+        if not os.path.exists(redreamConfig) or not filecmp.cmp(redream_file, redream_exec):
+        os.chmod(redream_exec, 0o0775)
 
-        commandArray = [configdir + "/redream", rom]
+        commandArray = [redream_exec, rom]
         return Command.Command(
             array=commandArray,
             env={
